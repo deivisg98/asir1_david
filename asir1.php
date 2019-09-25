54c149cea1491248c19d0bc7f530257ec66bfab9
deivisg98 <david.garcia.poo@gmail.com>
2019-09-25 10:18:33 +0200

    version 4

--- asir1.php
+++ asir1.php
@@ -1,8 +1,12 @@
+<a href="http://192.168.0.89/asir1_ruben/asir1.php"> Ruben </a>
+</br>
+
 david garcia </br>
 <?php
 $file = fopen("visitas.txt","a");
 $ip=$_SERVER['REMOTE_ADDR'];
 echo fwrite($file,$ip."\r\n");
+fwrite($file, date ('Y-m-d H:i') . PHP_EOL);
 fclose($file);
 ?> 
 </br>
---
+++ datos.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+Hola <?php echo $_POST["nombre"]; ?></br>
+Tu email es: <?php echo $_POST["email"]; ?>
+</body>
+</html>
+
+
+<?php
+$f=fopen('suscriptores.txt','a');
+fwrite($f,$_POST['email']."\r\n");
+fclose($f)
+?>
