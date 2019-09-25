ff59a2d993844d58c5d21ebf5ea0b0d1b5869394
deivisg98 <david.garcia.poo@gmail.com>
2019-09-25 08:32:03 +0200

    version 3

--- asir1.php
+++ asir1.php
@@ -1,8 +1,9 @@
-david garcia
-
+david garcia </br>
 <?php
 $file = fopen("visitas.txt","a");
 $ip=$_SERVER['REMOTE_ADDR'];
 echo fwrite($file,$ip."\r\n");
 fclose($file);
 ?> 
+</br>
+<a href="http://192.168.0.95/asir1_victorzayas/asir1.php"> Victor </a>
