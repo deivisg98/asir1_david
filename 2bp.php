48042fba13472a20d0bd51e8f339cfc64b73531c
deivisg98 <david.garcia.poo@gmail.com>
2019-10-02 08:32:43 +0200

    Version 2.bp

---
+++ 2bp.php
@@ -0,0 +1,10 @@
+<?php
+function calcula($a, $b){
+echo ($a + $b). '</br>';
+echo ($a - $b). '</br>';
+echo ($a * $b). '</br>';
+echo ($a / $b). '</br>';
+}
+echo calcula(2,3); 
+echo calcula(8,5);
+?>
