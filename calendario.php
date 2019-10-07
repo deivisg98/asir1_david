751ecfc5aa51bdae6aacbe8cc17fb700c0a5aad5
deivisg98 <david.garcia.poo@gmail.com>
2019-10-07 10:50:39 +0200

    Version 2.calendario

---
+++ calendario.php
@@ -0,0 +1,70 @@
+<?php
+function mes($n){
+	$a=[
+	[
+		'nombre' => 'enero',
+		'dia' => '31',
+		'estacion' => 'invierno'
+	],
+	[
+		'nombre' => 'febrero',
+		'dia' => '28',
+		'estacion' => 'invierno'
+	],
+	[
+		'nombre' => 'marzo',
+		'dia' => '31',
+		'estacion' => 'primavera'
+	],
+	[
+		'nombre' => 'abril',
+		'dia' => '30',
+		'estacion' => 'primavera'
+	],
+	[
+		'nombre' => 'mayo',
+		'dia' => '31',
+		'estacion' => 'primavera'
+	],
+	[
+		'nombre' => 'junio',
+		'dia' => '30',
+		'estacion' => 'verano'
+	],
+	[
+		'nombre' => 'julio',
+		'dia' => '31',
+		'estacion' => 'verano'
+	],
+	[
+		'nombre' => 'agosto',
+		'dia' => '31',
+		'estacion' => 'verano'
+	],
+	[
+		'nombre' => 'septiembre',
+		'dia' => '30',
+		'estacion' => 'otoño'
+	],
+	[
+		'nombre' => 'octubre',
+		'dia' => '31',
+		'estacion' => 'otoño'
+	],
+	[
+		'nombre' => 'noviembre',
+		'dia' => '30',
+		'estacion' => 'otoño'
+	],
+	[
+		'nombre' => 'diciembre',
+		'dia' => '31',
+		'estacion' => 'invierno'
+	]
+	];
+	return $a[$n-1];
+}
+echo mes(1)['nombre']."<br>";
+echo mes(1)['dia']."<br>";
+echo mes(1)['estacion']."<br>";
+?>
