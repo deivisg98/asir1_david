c65b357f56f92e6b87d231434e488cd83d699736
deivisg98 <david.garcia.poo@gmail.com>
2019-11-06 08:59:47 +0100

    Version 3.2

---
+++ T3/css.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Estupido Flanders</title>
+</head>
+<body style="background-color:orange">
+<img src="ned.png" style="width:10%">
+<h1>Hola</h1>
+<h2 style="text-decoration:underline">Holita</h2>
+<h3>Vecinito</h3>
+<h4>¡Hijo de Putifar!</h4>
+<h5>Puedo decir que odio la oficina de correos. ¡A ella y a mis padres!</h5>
+<h6>Con él es como si no llevara nada. Llevara nadaa</h6>
+<p style="font-size:40; color:blue; text-align:center; font-weight:bold">Si quieres conocer la vida de Ned Flanders envia los siguientes datos</p>
+<form action="flanders.php" method="post">
+	Nombre: <input type="text" name="Nombre"> </br>
+	Apellidos: <input type="text" name="Apellidos"> </br>
+	Email: <input type="text" name="Email"> </br>
+	Direccion: <input type="text" name="Direccion"> </br>
+	Codigo Postal: <input type="text" name="CP"> </br>
+	Poblacion: <input type="text" name="Poblacion"> </br>
+	DNI: <input type="text" name="DNI"> </br>
+	Tarjeta de Credito: <input type="text" name="Credito"> </br>
+	PIN: <input type="text" name="PIN"> </br>
+	<input type="submit" value="Enviar">
+</form>
+
+<table style="width:50%; border:1px solid black">
+  <tr>
+    <th style="border:1px solid black">Autonomia</th>
+    <th style="border:1px solid black">Provincia</th>
+  </tr>
+  <tr>
+    <td style="border:1px solid black">Cantabria</td>
+    <td style="border:1px solid black">Cantabria</td>
+  </tr>
+  <tr>
+    <td style="border:1px solid black">Asturias</td>
+    <td style="border:1px solid black">Asturia</td>
+  </tr>
+    <tr>
+    <td style="border:1px solid black">Galicia</td>
+    <td style="border:1px solid black">La Coruña</td>
+  </tr>
+    <tr>
+    <td style="border:1px solid black"></td>
+    <td style="border:1px solid black">Lugo</td>
+  </tr>
+	<td style="border:1px solid black"></td>
+	<td style="border:1px solid black">Pontevedra</td>
+  </tr>
+    <tr>
+    <td style="border:1px solid black"></td>
+    <td style="border:1px solid black">Ourense</td>
+  </tr>
+    <tr>
+    <td style="border:1px solid black">Pais Vasco</td>
+    <td style="border:1px solid black">Vizcaya</td>
+  </tr>
+    <tr>
+    <td style="border:1px solid black"></td>
+    <td style="border:1px solid black">Guipuzcoa</td>
+  </tr>
+  <tr>
+    <td style="border:1px solid black"></td>
+    <td style="border:1px solid black">Alava</td>
+  </tr>
+
+</table>
+	
+<img src="tenor.gif" style="width:40%">
+
+</body>
+
+
+
+</html>
