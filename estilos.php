7a637af0459ccb48c8f6561402a54976b2a9f6db
deivisg98 <david.garcia.poo@gmail.com>
2019-11-18 10:02:44 +0100

    Estilos.php

---
+++ estilos.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+<link rel="stylesheet" href="estilos.css">
+<title>Estupido Flanders</title>
+</head>
+<body id="cuerpo">
+<img id="imagen1" src="ned.png">
+<h1 class="letras">Frases de Ned Flanders:</h1>
+<h2 id="sub">Hola Holita Vecinito</h2>
+<h3 class="letras">Con él es como si no llevara nada. Llevara nadaa</h3>
+<h4 class="letras">¡Hijo de Putifar!</h4>
+<h5 class="letras">Puedo decir que odio la oficina de correos. ¡A ella y a mis padres!</h5>
+<p id="letritas">Si quieres conocer la vida de Ned Flanders envia los siguientes datos</p>
+<form action="Flanders.php" method="post">
+	Nombre: <input type="text" name="Nombre"> <br/>
+	Apellidos: <input type="text" name="Apellidos"> <br/>
+	Email: <input type="text" name="Email"> <br/>
+	Direccion: <input type="text" name="Direccion"> <br/>
+	Codigo Postal: <input type="text" name="CP"> <br/>
+	Poblacion: <input type="text" name="Poblacion"> <br/>
+	DNI: <input type="text" name="DNI"> <br/>
+	Tarjeta de Credito: <input type="text" name="Credito"> <br/>
+	PIN: <input type="text" name="PIN"> <br/>
+	<input type="submit" value="Enviar">
+</form>
+
+<table id="tabla">
+  <tr>
+    <th class="celda">Autonomia</th>
+    <th class="celda">Provincia</th>
+  </tr>
+  <tr>
+    <td class="celda">Cantabria</td>
+    <td class="celda">Cantabria</td>
+  </tr>
+  <tr>
+    <td class="celda">Asturias</td>
+    <td class="celda">Asturia</td>
+  </tr>
+    <tr>
+    <td class="celda">Galicia</td>
+    <td class="celda">La Coruña</td>
+  </tr>
+    <tr>
+    <td class="celda"></td>
+    <td class="celda">Lugo</td>
+  </tr>
+  <tr>
+	<td class="celda"></td>
+	<td class="celda">Pontevedra</td>
+  </tr>
+    <tr>
+    <td class="celda"></td>
+    <td class="celda">Ourense</td>
+  </tr>
+    <tr>
+    <td class="celda">Pais Vasco</td>
+    <td class="celda">Vizcaya</td>
+  </tr>
+    <tr>
+    <td class="celda"></td>
+    <td class="celda">Guipuzcoa</td>
+  </tr>
+  <tr>
+    <td class="celda"></td>
+    <td class="celda">Alava</td>
+  </tr>
+
+</table>
+	
+<img id="gif" src="tenor.gif">
+
+</body>
+
+
+
+</html>
