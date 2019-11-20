5fab9c9d405f456748cb18c74616f8d6d6e93e2d
deivisg98 <david.garcia.poo@gmail.com>
2019-11-20 09:23:54 +0100

    laboral e prueba otra

--- laboral_e.php
+++ laboral_e.php
@@ -1,25 +1,40 @@
 <?php
 
-$vacaciones=[
-'2019-10-28',
-'2019-11-01',
+// Declare two dates 
+$Date1 = '2019-10-28'; 
+$Date2 = '2019-11-01';
 
-'2019-12-06',
-'2019-12-09',
+//$Date3 = '2019-12-06';
+//$Date4 = '2019-12-09';
 
-'2019-12-23',
-'2020-01-07',
+//$Date5 = '2019-12-23';
+//$Date6 = '2020-01-07';
 
-'2020-02-24',
-'2020-02-28',
+//$Date7 = '2020-02-24';
+//$Date8 = '2020-02-28';
 
-'2020-04-09',
-'2020-04-17',
+//$Date9 = '2020-04-09';
+//$Date10 = '2020-04-17';
 
-'2020-05-01',
+//$Date11 = '2020-05-01';
+//$Date12 = '2020-06-25';
+  
+// Declare an empty array 
+$vacaciones = array(); 
+  
+// Use strtotime function 
+$Variable1 = strtotime($Date1); 
+$Variable2 = strtotime($Date2); 
+  
+// Use for loop to store dates into array 
+// 86400 sec = 24 hrs = 60*60*24 = 1 day 
+for ($currentDate = $Variable1; $currentDate <= $Variable2;  
+                                $currentDate += (86400)) { 
+                                      
+$Store = date('Y-m-d', $currentDate); 
+$vacaciones[] = $Store; 
+} 
 
-'2020-06-25',
-];
 
 for($i=1;$i<290;$i++){
 	$a=date('Y-m-d',($i-1)*24*60*60+strtotime('2019-09-13'));
