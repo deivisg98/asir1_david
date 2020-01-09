01e02132d5caa1d24672da4c9c8290b18cb14ef0
deivisg98 <david.garcia.poo@gmail.com>
2020-01-09 08:44:23 +0100

    consultas despues vacaciones

--- mysql.php
+++ mysql.php
@@ -12,7 +12,7 @@ if(0){
 
 $end= ['ana'];
 
-if(0){ 	// provincias de Galicia
+if(0){ 	// (00) provincias de Galicia
 	foreach($provincias as $p){
 		if($p['autonomia']=='Galicia'){
 			echo $p['provincia'];
@@ -22,7 +22,7 @@ if(0){ 	// provincias de Galicia
 }
 
 
-if(0){ // provincias que empiezan por A
+if(0){ // (09) provincias que empiezan por A
 	foreach($provincias as $p){
 		if($p['provincia'][0]=='A'){
 			echo $p['provincia'].'<br>';
@@ -31,7 +31,7 @@ if(0){ // provincias que empiezan por A
 }
 
 
-if(0){ // provincias con nombre compuesto
+if(0){ // (18) provincias con nombre compuesto
 	foreach($provincias as $p){
 		if(strstr($p['provincia'], ' ', true)){
 			echo $p['provincia'].'<br>';
@@ -40,7 +40,7 @@ if(0){ // provincias con nombre compuesto
 }
 
 
-if(0){ // provincias que estan en autonomias con nombre compuesto
+if(0){ // (17) provincias que estan en autonomias con nombre compuesto
 	foreach($provincias as $p){
 		if(strstr($p['autonomia'], ' ', true)){
 			echo $p['provincia'].'<br>';
@@ -49,7 +49,7 @@ if(0){ // provincias que estan en autonomias con nombre compuesto
 }
 
 
-if(0){ // provincias tienen nombre simple
+if(0){ // (19) provincias tienen nombre simple
 	foreach($provincias as $p){
 		if(!strstr($p['provincia'], ' ')){
 			echo $p['provincia'].'<br>';
@@ -58,7 +58,7 @@ if(0){ // provincias tienen nombre simple
 }
 
 
-if(0){ // provincias de galicia por poblacion (grande, mediana, pequeña)
+if(0){ // (51) provincias de galicia por poblacion (grande, mediana, pequeña)
 	foreach($provincias as $p){
 		if($p['autonomia']=='Galicia'){
 		if($p['poblacion']>1e6){
@@ -72,7 +72,7 @@ if(0){ // provincias de galicia por poblacion (grande, mediana, pequeña)
 }
 
 
-if(0) { // provincias que tienen el diptongo ue
+if(0) { // (08) provincias que tienen el diptongo ue
 	foreach($provincias as $p){
 				if(strstr($p['provincia'], 'ue', true)){
 			echo $p['provincia'].'<br>';
@@ -80,7 +80,7 @@ if(0) { // provincias que tienen el diptongo ue
 		}
 }
 
-if(0) { // autonomias que acaban en ana
+if(0) { // (10) autonomias que acaban en ana
 		foreach($provincias as $p){
 				foreach($end as $e) {
 					if(substr($p['autonomia'], -strlen($e)) === $e) {
@@ -90,7 +90,7 @@ if(0) { // autonomias que acaban en ana
 			}
 		}
 
-if(0) { // nombre y caracteres de cada autonomia (por orden descendente)
+if(0) { // (11) nombre y caracteres de cada autonomia (por orden descendente)
 	foreach($provincias as $p){
 				if(!strstr($p['autonomia'], '0', true)){
 			echo $p['autonomia'].' tiene '.strlen($p['autonomia']).' letras'.'<br>';
@@ -98,7 +98,7 @@ if(0) { // nombre y caracteres de cada autonomia (por orden descendente)
 		}
 	}
 	
-if(0) { // autonomias que empiezan por can
+if(0) { // (15) autonomias que empiezan por can
 		foreach($provincias as $p){
 					if(strstr($p['autonomia'], 'Can')) {
 				echo $p['autonomia'].'<br>';
@@ -106,11 +106,211 @@ if(0) { // autonomias que empiezan por can
 			}
 		}
 
-if(1){ // autonomias con provincias de mas de un millon de habitantes
+if(0){ // (16) autonomias con provincias de mas de un millon de habitantes
 	foreach($provincias as $p){
 		if($p['poblacion']>1e6){
 		echo $p['autonomia'].'<br>';}
 		}
 	}
+
+if(0){ // (21) poblacion del pais
+	$pob=0;
+	foreach($provincias as $p){
+		$pob+=$p['poblacion'];
+		echo $pob;
+	}
+}
+
+if(0) { // (23) cuantas provincias hay en la tabla
+		echo count($provincias);
+	}
+
+if(0) { // (01) listado de provincias
+	foreach ($provincias as $p) {
+		echo $p['provincia'].'<br>';
+	}
+}
+
+if(0) { // (02) cuanto es 2+3
+	echo 2+3;
+}
+
+if(0) { // (04) densidad de poblacion de las provincias
+	foreach($provincias as $p){
+		echo $p['provincia'].' '.$p['poblacion']/$p['superficie'].'<br>';
+	}
+}
+
+if(0) { // (52) listado de nombre de las provincias qeu tiene cada autonomia 
+	foreach($provincias as $p){
+		echo $p['autonomia'].' '.$p['provincia'].'<br>';
+	}
+}
+
+if(0) { // (05) caracteres de cada nombre de provincia
+	foreach($provincias as $p){
+		echo $p['provincia'].' '.strlen($p['provincia']).'<br>';
+	}
+}
+
+if(0) { // (06) listado de autonomias
+	foreach($provincias as $p){
+		$autonomia[]=$p['autonomia'];
+		$autonomia=array_unique($autonomia);
+		foreach ($autonomia as $a){
+			echo $a.'<br>';
+		}
+	}
+}
+
+if(0) { // (07) provincias con el mismo nombre que su comunidad autonoma
+	foreach($provincias as $p){
+		if ($p['provincia']==$p['autonomia']){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0) { // (25) autonomias que contienen el nombre de una de sus provincias
+	foreach($provincias as $p){
+			if(strstr($p['autonomia'], $p['provincia'])){
+				echo $p['autonomia'].'<br>';
+		}
+	}
+}
+
+if(0) { // (14) autonomia que tienen provincias con nombre compuesto
+	foreach($provincias as $p) 
+		if (strstr($p['provincia'], ' ')){
+			echo $p['autonomia'].'<br>';
+		}
+	}
+
+if(0) { // (22) superficie del pais
+	$s=0;
+	foreach ($provincias as $p){
+		echo $s+=$p['superficie'];
+	}
+}
+
+// despues de navidad
+
+if(0) { // 01
+	foreach ($provincias as $p){
+		echo $p['provincia'].'<br>';
+	}
+}
+
+if(0) { // 02
+		echo 2+3;
+}
+
+if(0) { // 04
+	foreach ($provincias as $p) {
+		echo $p['provincia'].' '.$p['poblacion']/$p['superficie'].'<br>';
+	}
+}
+
+if(0) { // 52
+	foreach ($provincias as $p){
+		echo $p['autonomia'].' '.$p['provincia'].'<br>';
+	}
+}
+
+if(0) { // 05
+	foreach($provincias as $p){
+		echo $p['provincia'].' '.strlen($p['provincia']).'<br>';
+	}
+}
+
+if(0){	// 06
+	foreach($provincias as $p)
+		$autonomias[]=$p['autonomia'];
+	$autonomias=array_unique($autonomias);
+	echo '<ol>';
+	foreach($autonomias as $a)
+		echo '<li>'.$a.'</li>';
+	echo '</ol>';
+}
+
+if(0){ // 07
+	foreach ($provincias as $p){
+		if($p['provincia']==$p['autonomia']){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 08
+	foreach($provincias as $p){
+		if(strstr($p['provincia'], 'ue', true)){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 09
+	foreach($provincias as $p){
+		if($p['provincia'][0]=='A'){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 18
+	foreach($provincias as $p){
+		if(strstr($p['provincia'],' ', true)){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 17
+	foreach($provincias as $p){
+		if(strstr($p['autonomia'],' ', true)){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 19
+	foreach($provincias as $p){
+		if(!strstr($p['provincia'],' ')){
+			echo $p['provincia'].'<br>';
+		}
+	}
+}
+
+if(0){ // 51
+	foreach($provincias as $p){
+		if($p['autonomia']=='Galicia'){
+		if($p['poblacion']>1e6){
+		echo $p['provincia'].' es grande'.'<br>';}
+		elseif($p['poblacion']>5e5){
+		echo $p['provincia'].' es mediana'.'<br>';}
+		else echo $p['provincia'].' es pequeña'.'<br>';
+		}
+	}
+}
+
+if(0){ // 10
+	foreach($provincias as $p){
+		foreach($end as $e){
+			if(substr($p['autonomia'], -strlen($e)) === $e){
+				echo $p['autonomia'].'<br>';
+			}
+		}
+	}
+}
+
+if(1){	// 11
+	foreach($provincias as $p){
+		$a=$p['autonomia'];
+		$as[$a]=iconv_strlen($a);
+	}
+	arsort($as);
+	foreach($as as $autonomia=>$longitud)
+		echo $autonomia.' '.$longitud.'<br/>';
+}
 
 ?>
